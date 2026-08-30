12e52f48fbda615b968c7997db6acd814185f358
Yanal-55 <202220216@ammanu.edu.jo>
2026-08-30 13:54:28 +0300

    Fix security scan findings and TruffleHog

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    host = os.getenv("FLASK_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_PORT", "5000"))
+
+    app.run(
+        debug=False,
+        host=host,
+        port=port
+    )
